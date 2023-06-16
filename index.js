8972522f4238a81aa599cd49c984645231b0f78f
r.stiglinc@gmail.com <r.stiglinc@gmail.com>
2023-06-16 10:24:34 +0200

    remove btn function and total sum

--- index.js
+++ index.js
@@ -29,15 +29,16 @@ function render() {
 
 render();
 
-let orderHtml = document.getElementById('order-process');
-let orderTitleHtml = document.querySelector('.title');
-let summaryEl = document.querySelector('#summary');
+const orderHtml = document.getElementById('order-process');
+const orderTitleHtml = document.querySelector('.title');
+const summaryEl = document.querySelector('#summary');
 
 document.addEventListener('click', function (e) {
   if (e.target.dataset.addBtn) {
     handleAddBtnClick(e.target.dataset.addBtn);
-    //handleRemoveBtnClick
-    //render order process Html
+  }
+  if (e.target.dataset.removeBtn) {
+    handleRemoveBtnClick(e.target.dataset.removeBtn);
   }
 });
 
@@ -53,25 +54,49 @@ function handleAddBtnClick(addBtnId) {
   orderProcess();
 }
 
+function handleRemoveBtnClick(removeBtnId) {
+  const targetRemoveBtn = menuArray.filter(function (menuItem) {
+    return menuItem.uuid === removeBtnId;
+  })[0];
+  targetRemoveBtn.numberOrdered--;
+  orderProcess();
+}
+
 const orderDisplay = () => {
   orderTitleHtml.classList.remove('hidden');
 };
 
 const orderProcess = function () {
   orderHtml.innerHTML = ``;
+  summaryEl.innerHTML = '';
+  let totalPrice = 0;
   menuArray.forEach(function (menu) {
     if (menu.numberOrdered > 0) {
       orderHtml.innerHTML += `
     <div class="order-wrapper">
         <p class="order-first">${menu.name}<button data-remove=${
         menu.uuid
-      } class="remove-btn">remove</button></p>
+      } class="remove-btn" data-remove-btn ="${menu.uuid}">remove</button></p>
         <p class="order-second">${menu.price * menu.numberOrdered}$</p>
     </div>`;
     }
-  });
-};
 
+    totalPrice += menu.price * menu.numberOrdered;
+  });
+  summaryEl.innerHTML = `
+  <div class="divider-total"></div>
+  <div class="total total-wrapper">
+    <h3>Total price:</h3>
+    <p>${totalPrice}$</p>
+  </div>
+  <button class="complete-order-btn wrapper" id="complete-order-btn">Complete Order</button>`;
+
+  if (totalPrice === 0) {
+    orderTitleHtml.classList.add('hidden');
+    orderHtml.innerHTML = ``;
+    summaryEl.innerHTML = '';
+  }
+};
 /* function processOrder() {
   let orderHtml = document.getElementById('order-process');
   let orderTitleHtml = document.querySelector('.title');
